6d0e38f28dba6a1758b80da2e2830f926b187acd
Jasson Moreira <jassonmoreiro@gmail.com>
2025-09-25 22:33:46 -0300

    Commit Desafio feito

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,25 +18,32 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            // FEITO: Buscar o Id no banco utilizando o EF
+            var tarefa = _context.Tarefas.Find(id);
+            // FEITO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
+           if (tarefa == null)
+            {
+                return NotFound();
+            }
+            return Ok(tarefa); // caso contrário retornar OK com a tarefa encontrada
+            
         }
 
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
+            // FEITO: Buscar todas as tarefas no banco utilizando o EF
+            var tarefa = _context.Tarefas.ToList();
             return Ok();
         }
 
         [HttpGet("ObterPorTitulo")]
         public IActionResult ObterPorTitulo(string titulo)
         {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
+            // FEITO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
+            var tarefa = _context.Tarefas.Where(x => x.Titulo.Contains(titulo));
+            return Ok(tarefa);
             // Dica: Usar como exemplo o endpoint ObterPorData
-            return Ok();
         }
 
         [HttpGet("ObterPorData")]
@@ -49,8 +56,6 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("ObterPorStatus")]
         public IActionResult ObterPorStatus(EnumStatusTarefa status)
         {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
             var tarefa = _context.Tarefas.Where(x => x.Status == status);
             return Ok(tarefa);
         }
@@ -61,7 +66,9 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            // FEITO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            var tarefaBanco = _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();          
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
@@ -76,8 +83,11 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
-            // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
+            // FEITO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
+            var tarefaAtualizada = tarefaBanco;
+            tarefaAtualizada.Titulo = tarefa.Titulo;
+            // FEITO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
+            var tarefaAtualizadaNoBanco = _context.Tarefas.Update(tarefaAtualizada);
             return Ok();
         }
 
@@ -90,6 +100,8 @@ namespace TrilhaApiDesafio.Controllers
                 return NotFound();
 
             // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
+            var tarefaRemovida = _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
             return NoContent();
         }
     }
